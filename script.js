11a5b910ef9c28cfc76db3dffb433ff9fef7cd43
Ray <qc@pop-os.localdomain>
2021-12-01 20:06:08 +0530

    copy exec added

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const generateBtn = document.querySelector('.generator');
 console.log(OTP);
 
 const randomNumber = () => {
-  const number = Math.floor(Math.random()*10000).toString();
-  console.log(number);
-   if(number.length > 3){
-     for(let i = 0; i < OTP.length; i++){
-         OTP[i].innerHTML=number[i]
-     }
-   }
-  
+  const number = Math.floor(Math.random() * 10000).toString();
+  const textarea = document.createElement('textarea');
+  document.body.appendChild(textarea);
+  textarea.style.visibility = 'hidden';
+  if (number.length > 3) {
+    textarea.innerHTML = number;
+    for (let i = 0; i < OTP.length; i++) {
+      OTP[i].innerHTML = number[i];
+    }
+    textarea.select();
 
+    document.execCommand('copy');
+  }
 };
 
 generateBtn.addEventListener('click', randomNumber);
